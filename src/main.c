daa507c1c8428dcca2538ba5d3b82ab6abf7d44b
Alexis <alexis.baret@gmx.fr>
2018-03-17 15:07:29 +0100

    [CDE] Upgrade main.c
    Build a tesintg prompt

--- src/main.c
+++ src/main.c
@@ -1,8 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define SUCCESS 0
+#define EXIT_CMD "exit"
+#define MAX 255
+
+int readStdin(char * buffer,int max_lenght){
+	int lenght;
+	fgets(buffer,max_lenght,stdin);
+	lenght = strlen(buffer);
+	buffer[lenght - 1] = '\0';
+	return lenght - 1;
+}
+void prompt(){
+	char cmd[255];
+	while(1){
+		printf(">> ");
+		readStdin(cmd,MAX);
+		if(strcmp(cmd,EXIT_CMD) == 0) break;
+	}
+}
 
 int main(){
+	prompt();
 	return SUCCESS;
 }
